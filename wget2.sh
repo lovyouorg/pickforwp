8f1f938eea07779654e7da6fa65b3b22339162ac
simon <13611113342@qq.com>
2019-08-21 11:22:30 +0800

    Create wget2.sh
    采集图片到nginx展示目录

---
+++ wget2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+arr=(5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61)
+for a in ${arr[*]}
+do
+ cd /mnt/www/gif/$a
+ grep 'http.*gif' 2.txt >url.txt
+ awk -F'"' '{print $4}' url.txt >url1.txt
+ wget -i url1.txt
+ sleep 30
+done
